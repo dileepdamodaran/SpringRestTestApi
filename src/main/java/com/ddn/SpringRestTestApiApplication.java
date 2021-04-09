934df3154d6a77a6003a9fdaababc800ac45d98e
ddn <dileep.damodaran.n@gmail.com>
2021-04-08 22:33:03 -0400

    First Commit

---
+++ src/main/java/com/ddn/SpringRestTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ddn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddn/SpringRestTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
